daf6e521331cbbae202fa8bb70d76c577428518b
AparneetDey <aparnitstudy@gmail.com>
2025-03-08 16:26:18 +0530

    vercel changes

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
 
 // Serve static files from the public directory
 app.use(express.static('public'));
 
 // Serve the index.html from the root directory
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port http://localhost:${port}`);
-});
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+    const port = 3000;
+    app.listen(port, () => {
+        console.log(`Server is running on port http://localhost:${port}`);
+    });
+}
+
+// Export the Express API
+module.exports = app;
