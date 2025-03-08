0307c33afbc4bfb84b62f34f2588eab98291bc39
AparneetDey <aparnitstudy@gmail.com>
2025-03-08 16:10:27 +0530

    Changes to all files and css

--- public/JS/carts/recipe-cart.js
+++ public/JS/carts/recipe-cart.js
@@ -1,47 +1,47 @@
 const recipes = [
     {
         name: "Spaghetti Bolognese",
-        image: "Images/spaghetti.jpg",
+        image: "/Images/spaghetti.jpg",
         description: "A classic Italian pasta dish with a rich, slow-cooked meat sauce."
     },
     {
         name: "Chicken Tikka Masala",
-        image: "Images/chicken-tikka.jpg",
+        image: "/Images/chicken-tikka.jpg",
         description: "Marinated chicken in a creamy, spiced curry sauce."
     },
     {
         name: "French Onion Soup",
-        image: "Images/french-onion-soup.jpg",
+        image: "/Images/french-onion-soup.jpg",
         description: "Deeply caramelized onions in a rich beef broth topped with melted cheese."
     },
     {
         name: "Pad Thai",
-        image: "Images/pad-thai.jpg",
+        image: "/Images/pad-thai.jpg",
         description: "Stir-fried rice noodles with tofu, peanuts, eggs, and a tangy tamarind sauce."
     },
     {
         name: "Chocolate Lava Cake",
-        image: "Images/choco-lava.jpg",
+        image: "/Images/choco-lava.jpg",
         description: "Rich chocolate cake with a gooey molten center."
     },
     {
         name: "Caesar Salad",
-        image: "Images/caesar-salad.jpg",
+        image: "/Images/caesar-salad.jpg",
         description: "Crisp romaine lettuce, Parmesan, croutons, and creamy Caesar dressing."
     },
     {
         name: "Japanese Ramen",
-        image: "Images/ramen.jpg",
+        image: "/Images/ramen.jpg",
         description: "Flavorful broth with noodles, soft-boiled eggs, and a variety of toppings."
     },
     {
         name: "Classic Cheeseburger",
-        image: "Images/burger.jpg",
+        image: "/Images/burger.jpg",
         description: "Juicy beef patty with cheddar cheese, lettuce, tomato, and pickles in a toasted bun."
     },
     {
         name: "Beef Stroganoff",
-        image: "Images/beef-stroganoff.jpg",
+        image: "/Images/beef-stroganoff.jpg",
         description: "Tender strips of beef cooked in a creamy mushroom sauce, served over egg noodles."
     }
 ];
--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
-app.set('views', path.join(__dirname));
-app.set('view engine', 'ejs');
+// Serve static files from the public directory
+app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Serve the index.html from the root directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
